7bb6876a6aef962b61846449b53dcb326d01889b
Caio Zanchetta Reis <caionohamachi@gmail.com>
2024-07-03 18:25:06 -0300

    Construção do JavaScript para conseguir criptografar e descriptografar o texto

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
-
+let criptografias = 0;
 
 function criptografar() {
-    alert("criptografado");
+    var origemTextArea = document.getElementById('origemTextArea').value;
+    var textoAlterado = origemTextArea
+        .replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+
+    document.getElementById('destinoTextArea').value = textoAlterado;
+    if(criptografias==0)
+        {
+            transformarTextoSecundario();
+        }
 }
 
 function descriptografar() {
-    alert("descriptografado");
+    var origemTextArea = document.getElementById('origemTextArea').value;
+    var textoAlterado = origemTextArea
+        .replace(/enter/g, 'e')
+        .replace(/ime/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+
+    document.getElementById('destinoTextArea').value = textoAlterado;
+    if(criptografias==0)
+        {
+            transformarTextoSecundario();
+        }
 }
 
 function copiar() {
-    alert("copiado");
+    var origemTextArea = document.getElementById('destinoTextArea').value;
+    var tempTextArea = document.createElement('textarea');
+    tempTextArea.value = origemTextArea;
+    document.body.appendChild(tempTextArea);
+    tempTextArea.select();
+    tempTextArea.setSelectionRange(0, 99999);
+    document.execCommand ('copy');
+    document.body.removeChild(tempTextArea);
+}
+
+function transformarTextoSecundario() {
+    document.getElementById("informacoes__secundario").style.display = "none";
+    document.getElementById("texto__secundario").style.display = "block";
 }
